f4013e608e504f9d60949a8a478a85e51f11f9d2
MoringaSchool <winnieadhiambo600@gmail.com>
2018-09-19 16:23:45 +0300

    add image in the file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("p").click(function() {
+    $("img").show();
+  });
+});
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
+
+
+    $(document).ready(function() {
+      $(".clickable").click(function() {
+        $("#walrus-showing").slideToggle();
+        $("#walrus-hidden").slideToggle();
+      });
+    });
